c2e044a37b55d3c994a1aaa4d01d4545837a546b
Sandeep Gupta <sangupta@adobe.com>
2017-10-17 10:03:11 -0700

    Added a basic Swatch class to hold a palette of colors

---
+++ src/main/java/com/sangupta/colors/Swatch.java
@@ -0,0 +1,57 @@
+package com.sangupta.colors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sangupta.jerry.util.AssertUtils;
+
+/**
+ * A swatch is an active palette of colors.
+ * 
+ * @author sangupta
+ *
+ */
+public class Swatch<T extends ColorModel> {
+
+	/**
+	 * Colors that are part of this {@link Swatch}
+	 */
+	public final List<T> colors = new ArrayList<>();
+	
+	/**
+	 * Create a new {@link Swatch} which is empty
+	 * 
+	 */
+	public Swatch() {
+		
+	}
+	
+	/**
+	 * Create a new {@link Swatch} and populate it with given colors.
+	 * 
+	 * @param colors
+	 */
+	public Swatch(T[] colors) {
+		if(AssertUtils.isEmpty(colors)) {
+			return;
+		}
+		
+		for(T color : colors) {
+			this.colors.add(color);
+		}
+	}
+
+	/**
+	 * Add the given {@link ColorModel} to the {@link Swatch}
+	 * 
+	 * @param color
+	 */
+	public void add(T color) {
+		if(color == null) {
+			return;
+		}
+		
+		this.colors.add(color);
+	}
+	
+}
